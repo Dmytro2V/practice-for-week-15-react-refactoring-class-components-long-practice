4d8e5f7f267ee22c87fc1e691fcda55e4631e3a0
Dmytro2V <op2vin@gmail.com>
2023-04-01 09:43:59 +0300

    done, test passed, no bonus

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {useState} from 'react';
 import Clock, { ClockToggle } from './components/Clock';
 import Folder from './components/Folder';
 import Weather from './components/Weather';
 import Autocomplete from './components/Autocomplete';
+
 
 const names = [
   'Abba',
@@ -21,27 +23,22 @@ const folders = [
   { title: 'three', content: 'Third folder here' }
 ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
-  }
+function App() {  
+  const [showClock, setShowClock] = useState(true)
   
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
+  const toggleClock = () => setShowClock(!showClock);
+
+ 
     return (
       <div className="widgets">
         <Folder folders={folders} />
         <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
+        <ClockToggle toggleClock={toggleClock} />
+        {showClock && <Clock />}
         <Autocomplete names={names} />
       </div>
     );
-  }
-}
+  
 
+}
 export default App;
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,49 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-export class ClockToggle extends React.Component {
-  render () {
+export function ClockToggle({toggleClock}) {
+  
     return (
       <button 
         type="button"
         className="clock-toggle" 
-        onClick={this.props.toggleClock}
+        onClick={toggleClock}
       >
         Toggle Clock
       </button>
     )
-  }
+  
 } 
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date(),
-    };
-  }
-  
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
-  
-  componentWillUnmount() {
-    console.log("Clearing Clock interval!")
-    clearInterval(this.interval);
-  }
-  
-  tick = () => {
-    this.setState({ time: new Date() });
+
+
+
+
+function Clock() {  
+  const [time, setTime] = useState(new Date())
+  const tick = () => {
+    setTime(new Date());
   }
 
-  render() {
-    let hours = this.state.time.getHours();
-    let minutes = this.state.time.getMinutes();
-    let seconds = this.state.time.getSeconds();
+  useEffect(()=>{
+    const interval = setInterval(tick, 1000);
+
+    return (
+      () => {
+        console.log("Clearing Clock interval!")
+        clearInterval(interval)}
+    )
+  },[])
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
     hours = (hours < 10) ? `0${hours}` : hours;
     minutes = (minutes < 10) ? `0${minutes}` : minutes;
     seconds = (seconds < 10) ? `0${seconds}` : seconds;
 
-    const timezone = this.state.time
+    const timezone = time
       .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
       .replace(/[^A-Z]/g, "") // Strip out all but capitals
       .slice(3); // Eliminate initial GMT
@@ -65,13 +62,13 @@ class Clock extends React.Component {
               Date: 
             </span>
             <span>
-              {this.state.time.toDateString()}
+              {time.toDateString()}
             </span>
           </p>
         </div>
       </section>
     );
-  }
+  
 }
 
 export default Clock;
